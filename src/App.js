b0b28147e7d4bbd32803c2c3666b5c7307903709
Ryan McGeeney <ryanmcgeeney@yahoo.com>
2020-01-04 12:37:23 -0600

    week 2 assignment added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Directory from './components/DirectoryComponent';
+import { CAMPSITES } from './shared/campsites';
           
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES
+    };
+  }
     render() {
         return (
             <div className="App">
@@ -12,8 +19,9 @@ class App extends Component {
                 <div className="container">
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
                 </div>
-                <Directory />
+               
                 </Navbar>
+                <Directory campsites={this.state.campsites} />
             </div>
         );
     }
