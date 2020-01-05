d2257961c59a409c4b82b1ec2827b97c32a0c8fd
Ryan McGeeney <ryanmcgeeney@yahoo.com>
2020-01-05 15:14:45 -0600

    Header and Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,8 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
-          
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';      
 
 class Main extends Component {
     constructor(props) {
@@ -27,8 +28,10 @@ class Main extends Component {
                 </div>
                
                 </Navbar>
+                <Header />
                 <Directory campsites={this.state.campsites} onClick = {campsiteId => this.onCampsiteSelect(campsiteId)} />
                 <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Footer />
             </div>
         );
     }
